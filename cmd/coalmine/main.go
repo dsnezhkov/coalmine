6b38ea4c62db3d2bed82e01a748156da9ca9401f
D.Snezhkov <dsnezhkov@gmail.com>
2021-11-16 18:24:43 -0600

    Updates

--- cmd/coalmine/main.go
+++ cmd/coalmine/main.go
@@ -5,36 +5,7 @@ import (
 )
 
 
-
-
 func main() {
 	cli.Execute()
-
-
-
-
-	/*dirname := flag.String("d", "", "Directory to fumigate")
-	filename := flag.String("f", "", "File to fumigate")
-	visuals := flag.Bool("v", true, "Visual progress")
-	 */
-
-	// flag.Parse()
-	// if *dirname == "" && *filename == "" {
-	// 	usage()
-	// 	return
-	// }
-	//
-	// cpdfm := cpdf.CPDFManagerFactory()
-	// if *dirname != "" {
-	// 	cpdfm.FumigateDir(*dirname, *visuals)
-	// }
-	// if *filename != "" {
-	// 	cpdfm.FumigateFile(*filename, *visuals)
-	// }
-	//
-	// cpdfm.PrintResults()
 }
 
-// func usage() {
-// 	fmt.Printf("Usage: coalmine -d=<dir>\n")
-// }
